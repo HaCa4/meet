e4f50a60fddf96ada2879d6c000c979fc51b5298
Çağrı <hcozagar@gmail.com>
2022-09-02 21:34:00 +0300

    time picker added

--- src/components/DateSelect.tsx
+++ src/components/DateSelect.tsx
@@ -1,58 +1,49 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 
 import { Button, Flex, Input, Text, Container } from "@chakra-ui/react";
 
 import { QuerySearchContext } from "../utils/context";
 
-import { useFormik } from "formik";
-
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+import { format } from "date-fns";
 const DateSelect: React.FC = () => {
   const { setEndDate, setStartDate, setIsQuerySelected } = useContext(QuerySearchContext);
 
-  const dateFormik = useFormik({
-    initialValues: {
-      startDate: "",
-      endDate: "",
-    },
-    onSubmit: (values) => {
-      setStartDate(values.startDate);
-      setEndDate(values.endDate);
-      setIsQuerySelected(true);
-    },
-  });
+  const [startDateFromPicker, setStartDateFromPicker] = useState(new Date());
+  const [endDateFromPicker, setEndDateFromPicker] = useState(new Date());
+
+  const onGoDateHandler = () => {
+    setStartDate(format(startDateFromPicker, "yyyy-MM-dd"));
+    setEndDate(format(endDateFromPicker, "yyyy-MM-dd"));
+    setIsQuerySelected(true);
+  };
 
   return (
-    <Container minW="800px" justifyContent={"center"} alignItems={"flex-start"}>
+    <Container minW="400px" justifyContent={"center"} alignItems={"flex-start"}>
       <Text w="100" color="blue.800" fontSize="xl" mb={2}>
         Pick a date range
       </Text>
       <Flex justifyContent={"space-evenly"} w="100%">
-        <Button colorScheme="green" onClick={() => setIsQuerySelected(false)}>
+        <Button px={"2rem"} colorScheme="green" onClick={() => setIsQuerySelected(false)}>
           Today
         </Button>
         <Text pt={"0.5rem"}>or</Text>
-        <form
-          style={{ width: "80%", display: "flex", justifyContent: "space-around" }}
-          onSubmit={dateFormik.handleSubmit}
-        >
-          <Input
-            name="startDate"
-            value={dateFormik.values.startDate}
-            onChange={dateFormik.handleChange}
-            w="35%"
-            placeholder="Start Date: YYYY-MM-DD"
+        <Flex>
+          <DatePicker
+            selected={startDateFromPicker}
+            onChange={(date: Date) => setStartDateFromPicker(date)}
+            className="picker"
           />
-          <Input
-            name="endDate"
-            value={dateFormik.values.endDate}
-            onChange={dateFormik.handleChange}
-            w="35%"
-            placeholder="End Date: YYYY-MM-DD"
+          <DatePicker
+            selected={endDateFromPicker}
+            onChange={(date: Date) => setEndDateFromPicker(date)}
+            className="picker"
           />
-          <Button type="submit" colorScheme="blue">
+          <Button px={"2rem"} onClick={() => onGoDateHandler()} colorScheme="blue">
             Go
           </Button>
-        </form>
+        </Flex>
       </Flex>
     </Container>
   );
